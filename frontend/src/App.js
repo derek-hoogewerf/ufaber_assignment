5638bbd1cf42f47ffea80f9dd6117266876cfbaa
Derek Hoogewerf <dbhoogewerf@gmail.com>
2021-05-19 13:50:53 +0530

    Create view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      taskList: tasks
 
+    };
+  }
+
+  tasks = [];
+
+  displayCompleted = status => {
+    if(status){
+      return this.setstatus({viewCompleted: true});
+    }  
+    else {
+      return this.setstatus({viewCompleted: false});
+    }
+  }
+
+  renderTabList = () => {
+    return (
+      <div className="my-5 tab-list">
+        <span
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
+        >
+          Completed
+        </span>
+        <span
+          onClick={()=> this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
+          >
+            Incomplete
+          </span>
+      </div>
+    )
+  }
+
+  // Rendering completed or imcomplete items
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.taskList.filter(
+      item => item.completed == viewCompleted
+    );
+
+    return newItems.map(item => (
+      <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+        <span className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`} title={item.name}>
+          {item.name}
+        </span>
+        <span>
+          <button className="btn btn-info mr-2"> Edit </button> 
+          <button className="btn btn-danger mr-2"> Delete </button>
+        </span>
+      </li>
+    ))
+  }
+
+  render() {
+    <main className="context">
+      <h1 className="text-black text-uppercase my-5"> Project Manager </h1>
+      <h3 className="row">
+        <div className="col-md-5 col-sma-10 mx-auto p-0">
+          <div className="card p-3">
+            <div>
+              <button  className="btn btn-primary"> Add Project </button>
+            </div>
+            {this.renderTabList()}
+            <ul className="list-group list-group-flush">
+              {this.renderItems()}
+            </ul>
+          </div>
+        </div>
+      </h3>
+    </main>
+  }
+}
 export default App;
