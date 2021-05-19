c37c0901ec8e94a7e72b28bcb6f37c90401c0f7f
Derek Hoogewerf <dbhoogewerf@gmail.com>
2021-05-19 05:55:22 +0530

    Add folder for static files

--- ufaber/start/admin.py
+++ ufaber/start/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "duration", "avatar", "completed")
 
 # Register your models here.
+admin.site.register(Project, ProjectAdmin)
--- ufaber/start/models.py
+++ ufaber/start/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length = 25)
+    name = models.CharField(max_length = 100)
     description = models.TextField()
     duration = models.DurationField()
     avatar = models.ImageField(upload_to = 'pics')
+    completed = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.name
 
