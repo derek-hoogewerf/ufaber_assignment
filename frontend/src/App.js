aa4332fc3c4c645870708c7ba9beb631bf817f0c
Derek Hoogewerf <dbhoogewerf@gmail.com>
2021-05-19 21:02:11 +0530

    Update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
 import React, {Component} from 'react'
 import './App.css';
+import CustomModal from './components/Modal'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewCompleted: false,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false
+      },
       taskList: tasks
-
     };
   }
 
-  tasks = [];
+// toggle property
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+  handleSubmit = item => {
+    this.toggle();
+    alert('Saved' + JSON.stringify(item));
+  }
+  handleDelete = item => {
+    alert('Deleted' + JSON.stringify(item));
+  }
+
+  createItem = () => {
+    const item = { title: "", modal: !this.state.modal };
+    this.setState({ activeItem: item, modal: !this.state.modal});
+  }
+
+  editItem = () => {
+    this.setState({ activeItem: item, modal: !this.state.modal});
+  }
 
   displayCompleted = status => {
     if(status){
-      return this.setstatus({viewCompleted: true});
+      return this.setState({viewCompleted: true});
     }  
     else {
-      return this.setstatus({viewCompleted: false});
+      return this.setState({viewCompleted: false});
     }
   }
 
@@ -62,22 +86,24 @@ class App extends Component {
   }
 
   render() {
-    <main className="context">
-      <h1 className="text-black text-uppercase my-5"> Project Manager </h1>
-      <h3 className="row">
-        <div className="col-md-5 col-sma-10 mx-auto p-0">
-          <div className="card p-3">
-            <div>
-              <button  className="btn btn-primary"> Add Project </button>
+    return (
+      <main className="context">
+        <h1 className="text-black text-uppercase my-5"> Project Manager </h1>
+        <h3 className="row">
+          <div className="col-md-5 col-sma-10 mx-auto p-0">
+            <div className="card p-3">
+              <div>
+                <button  className="btn btn-primary"> Add Project </button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
             </div>
-            {this.renderTabList()}
-            <ul className="list-group list-group-flush">
-              {this.renderItems()}
-            </ul>
           </div>
-        </div>
-      </h3>
-    </main>
+        </h3>
+      </main>
+    )
   }
 }
 export default App;
---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { 
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Input,
+    Label
+} from 'reactstrap';
+
+class CustomModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItem: this.props.activeItem
+        };
+    }
+
+// check if checkbox is checked
+    handleChange = e => {
+        let { name, value } = e.target;
+        if(e.target.type === "checkbox") {
+            value = e.target.checked;
+        }
+        const activItem = {...this.state.activeItem, [name]: value };
+        this.setState({ activItem })
+    };
+
+    render() {
+        const { toggle, onSave } = this.props;
+        return(
+            <Modal isOpen={true} toggle={toggle} >
+                <ModalHeader toggle={toggle}> Task Item </ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            {/* Project Name */}
+                            <label for="title"> Title </label>
+                            <Input 
+                            type="text" 
+                            name="name" 
+                            value={this.state.activeItem.name} 
+                            onChange={this.handleChange} 
+                            placeholder="Enter name of project" />
+                        </FormGroup>
+                        {/* Desription */}
+                        <FormGroup>
+                            <label for="description"> Description </label>
+                            <input 
+                            type="text"
+                            name="description"
+                            value={this.state.activeItem.description}
+                            onChange={this.handleChange}
+                            placeholder="Enter project description" />
+                        </FormGroup>
+                        {/*  */}
+                        <FormGroup check>
+                            <label for="completed"> Completed </label>
+                            <input 
+                            type="checkbox"
+                            name="completed"
+                            checked={this.state.activeItem.completed}
+                            onChange={this.handleChange} />
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <button color="success" onClick={() => onSave(this.state.activeItem)}>
+                        Save
+                    </button>
+                </ModalFooter>
+            </Modal>        
+        )
+    }
+}
+
+export default CustomModal
