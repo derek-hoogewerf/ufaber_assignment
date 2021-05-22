51fe2c7d53bc30d7227a1ddb670adebd5725daca
Derek Hoogewerf <dbhoogewerf@gmail.com>
2021-05-22 17:05:01 +0530

    Update admin.py

--- ufaber/start/admin.py
+++ ufaber/start/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Project
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "duration", "avatar", "completed")
+    list_display = ("name", "description", "start_date", "end_date", "completed")
 
 # Register your models here.
 admin.site.register(Project, ProjectAdmin)
