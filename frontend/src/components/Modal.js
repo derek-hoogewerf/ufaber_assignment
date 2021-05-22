13ff43dd6c682c7a2bad57325fe7bc1f75f61402
Derek Hoogewerf <dbhoogewerf@gmail.com>
2021-05-22 17:04:58 +0530

    Update Modal.js

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,80 +1,94 @@
 import React, { Component } from 'react';
-import { 
-    Button,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Form,
-    FormGroup,
-    Input,
-    Label
-} from 'reactstrap';
+// import React, { Component } from "react";
+// importing all of these classes from reactstrap module
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
 
 class CustomModal extends Component {
     constructor(props) {
-        super(props);
-        this.state = {
-            activeItem: this.props.activeItem
-        };
+      super(props);
+      this.state = {
+        activeItem: this.props.activeItem
+      };
     }
-
-// check if checkbox is checked
+    // changes handler to check if a checkbox is checed or not
     handleChange = e => {
-        let { name, value } = e.target;
-        if(e.target.type === "checkbox") {
-            value = e.target.checked;
-        }
-        const activItem = {...this.state.activeItem, [name]: value };
-        this.setState({ activItem })
+      let { name, value } = e.target;
+      if (e.target.type === "checkbox") {
+        value = e.target.checked;
+      }
+      const activeItem = { ...this.state.activeItem, [name]: value };
+      this.setState({ activeItem });
     };
-
+  
+    // rendering modal in the custommodal class received toggle and on save as props,
+    // toggle is a function used to control the Modal’s state i.e open or close the modal
+    // onSave is a function that is called to save the edited values of the task item.
     render() {
-        const { toggle, onSave } = this.props;
-        return(
-            <Modal isOpen={true} toggle={toggle} >
-                <ModalHeader toggle={toggle}> Task Item </ModalHeader>
-                <ModalBody>
-                    <Form>
-                        <FormGroup>
-                            {/* Project Name */}
-                            <label for="title"> Title </label>
-                            <Input 
-                            type="text" 
-                            name="name" 
-                            value={this.state.activeItem.name} 
-                            onChange={this.handleChange} 
-                            placeholder="Enter name of project" />
-                        </FormGroup>
-                        {/* Desription */}
-                        <FormGroup>
-                            <label for="description"> Description </label>
-                            <input 
-                            type="text"
-                            name="description"
-                            value={this.state.activeItem.description}
-                            onChange={this.handleChange}
-                            placeholder="Enter project description" />
-                        </FormGroup>
-                        {/* completed or not boolean */}
-                        <FormGroup check>
-                            <label for="completed"> Completed </label>
-                            <input 
-                            type="checkbox"
-                            name="completed"
-                            checked={this.state.activeItem.completed}
-                            onChange={this.handleChange} />
-                        </FormGroup>
-                    </Form>
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="success" onClick={() => onSave(this.state.activeItem)}>
-                        Save
-                    </Button>
-                </ModalFooter>
-            </Modal>        
-        )
+      const { toggle, onSave } = this.props;
+      return (
+        <Modal isOpen={true} toggle={toggle}>
+          <ModalHeader toggle={toggle}> Project </ModalHeader>
+          <ModalBody>
+            {/* In the modalbody we can create a form for title, description, and completed ; he same fields that we defined as properties on the Todo model in the backend , in the models.py file */}
+            <Form>
+  
+              {/* 3 formgroups
+              1 title label */}
+              <FormGroup>
+                <Label for="title">Project</Label>
+                <Input
+                  type="text"
+                  name="title"
+                  value={this.state.activeItem.title}
+                  onChange={this.handleChange}
+                  placeholder="Enter Project Title"
+                />
+              </FormGroup>
+  
+              {/* 2 description label */}
+              <FormGroup>
+                <Label for="description">Description</Label>
+                <Input
+                  type="text"
+                  name="description"
+                  value={this.state.activeItem.description}
+                  onChange={this.handleChange}
+                  placeholder="Enter Project Description"
+                />
+              </FormGroup>
+  
+              {/* 3 completed label */}
+              <FormGroup check>
+                <Label for="completed">
+                  <Input
+                    type="checkbox"
+                    name="completed"
+                    checked={this.state.activeItem.completed}
+                    onChange={this.handleChange}
+                  />
+                  Completed
+                </Label>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+          {/* create a modal footer */}
+          <ModalFooter>
+            <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+              Save
+            </Button>
+          </ModalFooter>
+        </Modal>
+      );
     }
-}
-
-export default CustomModal
+  }
+  export default CustomModal
